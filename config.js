6165de457e512e680dad9c176803b60f33b44912
Momo kell <kellmomo055@gmail.com>
2023-10-23 05:14:33 +0100

    javaScript file 2

---
+++ config.js
@@ -0,0 +1,3 @@
+//listes de mots pour le jeu
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique!", "La vie,L'univers et le reste", "Merci pour le poisson"]
